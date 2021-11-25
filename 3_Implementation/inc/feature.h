4afed093f3eebef71f533c952ab0f7ab92047271
vin099 <94450629+vin099@users.noreply.github.com>
2021-11-25 09:24:46 +0530

    Update feature.h

--- 3_Implementation/inc/feature.h
+++ 3_Implementation/inc/feature.h
@@ -1,1 +1,17 @@
+#ifndef __FEATURE_H
+#define __FEATURE_H
 
+
+#include<stdio.h>
+#include<stdlib.h>
+char matrix[]
+
+
+void display()
+void userInput(int)
+char checkWin()
+void compInput(int)
+int main()
+  
+  
+#end if
